181c61aad1ca1695a39e6989c4e31fa9ad4c8af3
dog <dog@126.com>
2015-07-27 15:50:49 +0800

    初始化项目

---
+++ dididaren/dididaren/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dididaren
+//
+//  Created by apple on 15/7/27.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dididaren/dididaren/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  dididaren
+//
+//  Created by apple on 15/7/27.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
